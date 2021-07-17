ee347456ab7f179be6e5f20b8e29672150a9ef9f
liyijie <liyijie825@gmail.com>
2021-07-17 20:55:52 +0800

    use autoload for responder

--- lib/simple_controller.rb
+++ lib/simple_controller.rb
@@ -5,9 +5,9 @@ require 'responders'
 require 'ransack'
 require 'inherited_resources'
 require 'pundit'
-require 'simple_controller/responder'
 
 module SimpleController
   autoload :VERSION,            'simple_controller/version'
   autoload :BaseController,               'simple_controller/base_controller'
+  autoload :Responder,               'simple_controller/responder'
 end
