0e80481eb9cd6307f74f6d0be52d375711c9ba6a
liyijie <liyijie825@gmail.com>
2021-07-29 12:37:22 +0800

    update simple_controller responder without responder cache

--- lib/simple_controller/responder.rb
+++ lib/simple_controller/responder.rb
@@ -1,6 +1,6 @@
 class SimpleController::Responder < ActionController::Responder
   include Responders::FlashResponder
-  include Responders::HttpCacheResponder
+  # include Responders::HttpCacheResponder
 
   def json_resource_errors
     { errors: resource.errors, message: resource.errors.map { |error| error.message}.join('\n') }
