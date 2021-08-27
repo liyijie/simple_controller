d702c31504b17c88982c880e00a24f6570ea7f8b
liyijie <liyijie825@gmail.com>
2021-08-27 08:00:28 +0800

    simple_controller autoload exportable & importable controller

--- lib/simple_controller.rb
+++ lib/simple_controller.rb
@@ -9,5 +9,7 @@ require 'pundit'
 module SimpleController
   autoload :VERSION,            'simple_controller/version'
   autoload :BaseController,               'simple_controller/base_controller'
+  autoload :Exportable,               'simple_controller/exportable'
+  autoload :Importable,               'simple_controller/importable'
   autoload :Responder,               'simple_controller/responder'
 end
