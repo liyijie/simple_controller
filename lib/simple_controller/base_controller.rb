5c3341c5399196e12c2d7f0d0a3d496635efa8f3
liyijie <liyijie825@gmail.com>
2021-09-12 18:09:44 +0800

    scope_policy_class use in policy_association_chain

--- lib/simple_controller/base_controller.rb
+++ lib/simple_controller/base_controller.rb
@@ -201,9 +201,8 @@ class SimpleController::BaseController < ::InheritedResources::Base
 
   def policy_association_chain
     policy_class ||= self.class.instance_variable_get(:@policy_class)
-    scope_policy_class = "#{policy_class}::Scope".safe_constantize
     if policy_class.present? &&
-        scope_policy_class &&
+        (scope_policy_class = "#{policy_class}::Scope".safe_constantize) &&
         origin_end_of_association_chain.is_a?(ActiveRecord::Relation)
       scope_policy_class.new(current_user, origin_end_of_association_chain).resolve
     else
