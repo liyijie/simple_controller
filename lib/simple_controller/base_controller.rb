ef8301ab2c649163e2376b1bc04c75b0040e9516
liyijie <liyijie825@gmail.com>
2021-12-07 19:49:45 +0800

    fix: 修复assocaition chain的各个顺序

--- lib/simple_controller/base_controller.rb
+++ lib/simple_controller/base_controller.rb
@@ -223,12 +223,12 @@ class SimpleController::BaseController < ::InheritedResources::Base
     end
   end
 
-  def after_of_association_chain
-    after_association_chain(query_association_chain)
+  def end_of_association_chain
+    query_association_chain
   end
 
-  def end_of_association_chain
-    _association_chain = after_of_association_chain
+  def after_of_association_chain
+    _association_chain = after_association_chain(query_association_chain)
     if _association_chain.respond_to?(:order)
       _association_chain.order(id: :desc)
     else
